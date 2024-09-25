7667f740877a679aded296c408db96c431d52a9e
André Plöger <andre.ploeger@gmx.de>
2024-09-25 11:11:40 -0400

    code cleanup

--- redirect.py
+++ redirect.py
@@ -4,7 +4,6 @@ import os
 import requests
 from requests.exceptions import RequestException
 
-# Konfiguration
 TIMEOUT = 0.1
 DEFAULT_LANGUAGE = 'en'
 WEBSITE_URLS = {
@@ -12,12 +11,10 @@ WEBSITE_URLS = {
     'en': 'http://www.andreploeger.com/en/'
 }
 
-# Mapping von Ländern zu Sprachen
 COUNTRY_LANGUAGE_MAP = {
     'DE': 'de',
-    'AT': 'de',  # Österreich
-    'CH': 'de',  # Schweiz
-    # Weitere Länder können hier hinzugefügt werden
+    'AT': 'de',
+    'CH': 'de',
 }
 
 PROVIDERS = [
@@ -26,17 +23,16 @@ PROVIDERS = [
     'https://ipgeolocation.io/ip-api/{}/json/',
 ]
 
-# Header für die HTTP-Antwort
 print("Content-Type: text/html")
 print("Status: 302 Found")
-print("Location: http://www.andreploeger.com/en/")  # Standard-Weiterleitung
-print()  # Leere Zeile für die Header
+print("Location: http://www.andreploeger.com/en/")
+print()
 
 def get_user_language():
     lang = os.environ.get('HTTP_ACCEPT_LANGUAGE', '')
     if lang:
         return lang.split(',')[0].split(';')[0]
-    return None  # Keine Sprache ermittelt
+    return None
 
 def get_language_from_ip(ip):
     for provider in PROVIDERS:
@@ -48,19 +44,16 @@ def get_language_from_ip(ip):
                 if country in COUNTRY_LANGUAGE_MAP:
                     return COUNTRY_LANGUAGE_MAP[country]
         except RequestException:
-            continue  # Nächster Anbieter bei Fehler
+            continue
 
-    return DEFAULT_LANGUAGE  # Standard auf Englisch
+    return DEFAULT_LANGUAGE
 
-# Ermitteln der Sprache
 user_language = get_user_language()
 
 if not user_language:
-    user_ip = os.environ.get('REMOTE_ADDR', '0.0.0.0')  # IP des Benutzers
+    user_ip = os.environ.get('REMOTE_ADDR', '0.0.0.0')
     user_language = get_language_from_ip(user_ip)
 
-# Weiterleitung basierend auf der ermittelten Sprache
 redirect_url = WEBSITE_URLS.get(user_language, WEBSITE_URLS[DEFAULT_LANGUAGE])
 print(f"Location: {redirect_url}")
-
-print()  # Leere Zeile für die Header
+print()
